73d30c1fbd264e552f759d1e3dde5d6f3d88f6e0
cineprod <142513195+cine-production@users.noreply.github.com>
2023-10-17 18:23:54 +0200

    url

--- CustomCoders/script.js
+++ CustomCoders/script.js
@@ -17,7 +17,7 @@ checkPasswordButton.addEventListener('click', () => {
     const password = passwordInput.value;
 
     if (password === 'JulieVogt123') {
-        window.location.href = 'https://cine-production.github.io/ServiceTiers/lienClient/JulieVogt/'; // Rediriger vers la première page
+        window.location.href = 'https://cine-production.github.io/ServiceTiers/CustomCoders/CodeTerminer/JulieVogt/'; // Rediriger vers la première page
     } else if (password === 'ent123') {
         window.location.href = 'page2.html'; // Rediriger vers la deuxième page
     } else {
