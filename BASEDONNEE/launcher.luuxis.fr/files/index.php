aad5960bf670e5a861cc3a53e2abdf5ac9ce1b23
cineprod <soundtrackdlp.contact@gmail.com>
2024-05-11 11:52:05 +0200

    e

--- BASEDONNEE/launcher.luuxis.fr/files/index.php
+++ BASEDONNEE/launcher.luuxis.fr/files/index.php
@@ -1,35 +1,20 @@
-<?php
-header("Content-Type: application/json; charset=UTF-8");
-include 'php/scandir.php';
-
-$instance_param = $_GET['instance'] ?? 'null';
-
-if ($instance_param == '/' || $instance_param[0] == '.') {
-    echo json_encode([]);
-    exit;
-} 
-
-if (!file_exists('instances')) {
-    echo dirToArray("files");
-    exit;
-} 
-
-if ($instance_param == 'null') {
-    $instances_list = scanFolder("instances");
-    $instance = array();
-    foreach ($instances_list as $value) {
-        if (substr($_SERVER['REQUEST_URI'], -1) == '/') {
-            $_SERVER['REQUEST_URI'] = substr($_SERVER['REQUEST_URI'], 0, -1);
+{
+    "Fabric-1.20": {
+        "name": "Fabric-1.20",
+        "url": "http://launcher.luuxis.fr/files?instance=McW3",
+        "loadder": {
+            "minecraft_version": "1.16.5",
+            "loadder_type": "Forge",
+            "loadder_version": "latest"
+        },
+        "verify": false,
+        "ignored": [],
+        "whitelist": [],
+        "whitelistActive": false,
+        "status": {
+            "nameServer": "Hypixel",
+            "ip": "mc.hypixel.net",
+            "port": 25565
         }
-
-        $url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]?instance=$value";
-        $instance[$value] = array("name" => $value, "url" => $url);
     }
-    
-    include 'php/instances.php';
-    echo str_replace("\\", "", json_encode($instance));
-    exit;
 }
-
-echo dirToArray("instances/$instance_param");
-?>
