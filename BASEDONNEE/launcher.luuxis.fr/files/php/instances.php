8c03a66d569bf219fe56a2cd25798f4d27260e36
cineprod <soundtrackdlp.contact@gmail.com>
2024-05-11 11:38:04 +0200

    h

---
+++ BASEDONNEE/launcher.luuxis.fr/files/php/instances.php
@@ -0,0 +1,20 @@
+{
+    "Fabric-1.20": {
+        "name": "Fabric-1.20",
+        "url": "http://launcher.luuxis.fr/files?instance=McW3",
+        "loadder": {
+            "minecraft_version": "1.16.5",
+            "loadder_type": "Forge",
+            "loadder_version": "latest"
+        },
+        "verify": false,
+        "ignored": [],
+        "whitelist": [],
+        "whitelistActive": false,
+        "status": {
+            "nameServer": "Hypixel",
+            "ip": "mc.hypixel.net",
+            "port": 25565
+        }
+    }
+}
